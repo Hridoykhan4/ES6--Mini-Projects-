290a4ad718624526447157a094c438174108b807
Hridoykhan4 <hridoykhan148385@gmail.com>
2025-02-15 14:14:44 +0600

    calculator set up

--- js/calculator.js
+++ js/calculator.js
@@ -1,51 +1,75 @@
-/* const darkTag = document.getElementsByTagName("html")[0];
-darkTag.setAttribute("data-theme", "light");
-
 document.addEventListener("DOMContentLoaded", () => {
-  const buttons = document.querySelectorAll("input[type='button']");
-
-  const display = document.getElementById("display");
+  const buttons = document.querySelectorAll('input[type="button"]');
+  const display = document.querySelector("#display");
 
   buttons.forEach((button) => {
     button.addEventListener("click", () => {
-      const value = button.value;
-
-      if (value === "AC") {
+      const operators = ["+", "-", "*", "/"];
+      const lastChar = display.value.slice(-1);
+      const data = button.value;
+      if (data === "AC") {
         display.value = "";
-      } else if (value === "DE") {
-        const finalValue = display.value.slice(0, display.value.length - 1);
-        display.value = finalValue;
-      } else if (value === "=") {
-        try {
-          display.value = eval(display.value);
-        } catch (e) {
-          display.value = "Error";
-        }
-      } else {
-        display.value += value;
+      } else if (data === "DE") {
+        display.value = display.value.slice(0, -1);
+      } else if (data === "=") {
+        calculateResult();
+      }else if(!isNaN(data)){
+        appendNumber(data);
       }
+      else if(data === '.'){
+        if(!display.value.includes(data)){
+          appendNumber(data)
+        }
+      }
+      else if(operators.includes(data)){
+        if(!operators.includes(lastChar)){
+          display.value += data
+        }
+      }
+
     });
   });
-}); */
+});
 
-/* document.addEventListener("DOMContentLoaded", () => {
-  const button = document.querySelectorAll("input[type='button']");
-  const display = document.getElementById("display");
-  
-  button.forEach((button) => {
-    button.addEventListener('click', function(){
-      const value = button.value;
-      if(value === 'AC'){
-        display.value = ''
-      }else if(value === 'DE'){
-        display.value = ''
+
+const appendNumber = (val) => {
+  display.value += val;
+};
+
+const calculateResult = () => {
+  try {
+    if(display.value.trim() !== ''){
+      display.value = eval(display.value);
+    }
+  } catch (err) {
+    display.value += err;
+  }
+};
+
+
+document.addEventListener('keyup', (e) => {
+  const target = e.key;
+  const lastChar = display.value.slice(-1);
+  const operators = ["+", "-", "*", "/", "."];
+
+  if(!isNaN(target)){
+    appendNumber(target)
+  }
+  else if(operators.includes(target)){
+      if(!operators.includes(lastChar)){
+        appendNumber(target)
       }
-      else if(value === '='){
-        display.value = eval(display.value)
-      }
-      else{
-        display.value = display.value + value
-      }
-    })
-  })
-}) */
+  }
+  else if(target === 'Enter'){
+    calculateResult()
+  }else if(target === 'Backspace'){
+    display.value = display.value.slice(0, -1)
+  }
+  else if(target === 'Escape'){
+    display.value = ''
+  }
+ if(e.code === 'Space'){
+  display.value =  display.value.trim()
+}
+})
+
